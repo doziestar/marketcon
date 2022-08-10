d4552f2edadc4c21db8cd37e86ac578c439d1c9e
Daniel Don <dondaniel028@gmail.com>
2022-08-10 18:01:27 +0100

    Start Dashboard layout

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import "./styles/index.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
 	return (
@@ -11,6 +12,7 @@ function App() {
 				<Routes>
 					<Route path={"/"} element={<Signup />} />
 					<Route path={"/login"} element={<Login />} />
+					<Route path={"/dashboard"} element={<Dashboard />} />
 				</Routes>
 			</Router>
 		</div>
---
+++ client/src/pages/Dashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../layouts/MainLayout";
+
+function Dashboard() {
+	return (
+		<div>
+			<Layout>Lorem</Layout>
+		</div>
+	);
+}
+
+export default Dashboard;
